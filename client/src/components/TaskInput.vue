039bc80a99a9278950bfeb02e06d4706c4553181
Jose Rodolfo Nieblas <joserodolfonieblas@gmail.com>
2024-02-26 09:11:08 -0800

    feat(client)
    - added input and button
    - styled input and button

--- client/src/components/TaskInput.vue
+++ client/src/components/TaskInput.vue
@@ -1,13 +1,11 @@
-<template>
-  <div class="task-input">
-    <!--
+<!--
       Mission One: Add a task input
 
       Create a task input according to the design in the README.md file.
 
       Definition of done:
-      [ ] User can type into the input
-      [ ] A button with the text "Add" is visible
+      [x] User can type into the input
+      [x] A button with the text "Add" is visible
       [ ] User can click the button to submit the input
        
       Your submission will be judged out of ten points based
@@ -20,14 +18,75 @@
         - Are concerns clearly separeted? (e.g. clear responsibilities for functions, etc.) - 3 point
         - Is the code easy to read and understand? - 2 points
     -->
+
+<template>
+  <div class="task-input">
+    <div id="input-task-wrapper">
+      <input type="text" placeholder="Add a task" />
+      <button>Add</button>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-/**
+<!-- 
  * Component design is up to you. You'll define the props,
  * events, and methods for the component here.
  * 
  * Good luck!
- */
+-->
+<script setup lang="ts">
+
 </script>
+
+<style scoped lang="scss">
+  div#input-task-wrapper{
+    position: relative;
+  }
+  // For input inside the wrapper.
+  div#input-task-wrapper input{
+    width: 100%;
+    padding: var(--space-2) var(--space-3); 
+    font-size: var(--fs-3); 
+    font-family: "Open Sans", sans-serif;
+    color: var(--color-text);
+    border-radius: var(--border-radius-1);
+    border: 2px solid var(--color-border);
+    background-color: var(--color-input-background);
+  }
+  div#input-task-wrapper input:focus{
+    outline: none;
+  }
+
+  // For the input placeholder inside wrapper.
+  div#input-task-wrapper input::placeholder{
+    font-style: italic;
+    color: var(--color-text);
+  }
+  div#input-task-wrapper input:focus::placeholder{
+    color: transparent;
+  }
+  div#input-task-wrapper input:placeholder-shown:hover{
+    cursor: pointer;
+  }
+
+  // For the button inside the wrapper.
+  div#input-task-wrapper button{
+    position: absolute;
+    right: var(--space-2);
+    top: 9px; //Centered button, calculated((42.4 - 24.08) / 2) = 9.16 or could also use flexbox if desired
+    font-weight: bold;
+    letter-spacing: 2px;
+    background-color: var(--color-primary);
+    color: var(--color-text);
+    border-radius: var(--border-radius-1);
+    cursor: pointer;
+    border: none;
+    box-shadow: var(--box-shadow-1);
+    padding: var(--space-1) var(--space-3);
+    font-size: var(--fs-2); 
+    background-color: var(--theme-c-white);
+  }
+  div#input-task-wrapper button:active{
+    color: var(--color-accent-one);
+  }
+</style>
